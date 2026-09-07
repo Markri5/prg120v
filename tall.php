66f2ec677b719fe61bcf59021b9bd88ac0dc3cc3
MarKri <awarlace@gmail.com>
2026-09-07 14:53:28 +0200

    Add basic calculator for summing and subtracting numbers
    Implement basic calculator functionality to sum and subtract two numbers from POST data.

--- tall.php
+++ tall.php
@@ -1,1 +1,15 @@
+<?php    /* Navn eksempel */
+/*
+Dette er tall kalkulator som henter info fra filen index.html
+*/
+  $tall1=$_POST ["tall1"];
+  $tall2=$_POST ["tall2"];  
 
+  $summen=$tall1 + $tall2;  
+  $differansen=$tall1 - $tall2;  
+			
+  print ("Tall 1 er $tall1 <br />");
+  print ("Tall 2 er $tall2 <br />");
+  print ("Summen er $summen <br />");
+  print ("Differansen er $differansen <br />"); 
+?>
