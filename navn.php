00020ebd7a936278f14c7db085eab06e17d9666f
MarKri <awarlace@gmail.com>
2026-09-07 14:14:02 +0200

    Correct variable usage in navn.php
    Fix variable assignment for last name and print greeting.

---
+++ navn.php
@@ -0,0 +1,9 @@
+<?php    /* Navn eksempel */
+/*
+Dette henter fra boksene der du fyller inn Fornavn og etternavn i filen index.html
+*/
+  $fornavn=$_POST ["fornavn"];
+  $fornavn=$_POST ["etternavn"];
+
+  print ("God dag $fornavn $etternavn <br/>");
+?>
