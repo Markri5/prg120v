96679e0fc4d3e3785bceb71342a6d58072a0ace8
MarKri <awarlace@gmail.com>
2026-09-14 14:46:26 +0200

    Implement basic calculator functionality in index.php
    Added a simple calculator that performs addition and subtraction of two numbers from user input.

--- index.php
+++ index.php
@@ -42,3 +42,19 @@ Dette henter fra boksene der du fyller inn Fornavn og etternavn i filen index.ht
 
   print ("God dag $fornavn $etternavn <br />");
 ?>
+
+<?php    /* Navn eksempel */
+/*
+Dette er tall kalkulator som henter info fra filen index.html
+*/
+  $tall1=$_POST ["tall1"];
+  $tall2=$_POST ["tall2"];  
+
+  $summen=$tall1 + $tall2;  
+  $differansen=$tall1 - $tall2;  
+			
+  print ("Tall 1 er $tall1 <br />");
+  print ("Tall 2 er $tall2 <br />");
+  print ("Summen er $summen <br />");
+  print ("Differansen er $differansen <br />"); 
+?>
