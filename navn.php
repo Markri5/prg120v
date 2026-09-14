4e23c4ba60a9b46e689daba0a9e971d6ea5298b1
MarKri <awarlace@gmail.com>
2026-09-14 14:54:13 +0200

    Delete navn.php

--- navn.php
+++
@@ -1,9 +0,0 @@
-<?php    /* Navn eksempel */
-/*
-Dette henter fra boksene der du fyller inn Fornavn og etternavn i filen index.html
-*/
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];
-
-  print ("God dag $fornavn $etternavn <br />");
-?>
